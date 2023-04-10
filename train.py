a803af12c034e018c4d5b1adb9d17a41eaf7634d
oahzxl <xuanlei.zhao@gmail.com>
2023-04-10 19:53:00 +0800

    init

---
+++ train.py
@@ -0,0 +1,40 @@
+import torch.nn as nn
+import torch.optim as optim
+
+
+from model import Net
+from dataset import get_data
+
+
+def main():
+    train_loader, test_loader = get_data()
+    model = Net()
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    for epoch in range(2):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(train_loader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+
+            optimizer.zero_grad()
+
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
+
+    print('Finished Training')
+
+
+if __name__ == "__main__":
+    main()
