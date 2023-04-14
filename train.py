9535583d12c9d220eb80affbd6649d1dd8c0261e
oahzxl <xuanlei.zhao@gmail.com>
2023-04-14 16:01:06 +0800

    update code

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.optim as optim
 
@@ -10,11 +11,12 @@ def main():
     model = Graphormer(64, 4, 2).cuda()
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = optim.Adam(model.parameters())
 
-    for epoch in range(2):  # loop over the dataset multiple times
+    for epoch in range(5):  # loop over the dataset multiple times
 
         running_loss = 0.0
+        model.train()
         for i, data in enumerate(train_loader, 0):
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
@@ -31,8 +33,31 @@ def main():
             # print statistics
             running_loss += loss.item()
             if i % 2000 == 1999:  # print every 2000 mini-batches
-                print(f"[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}")
+                print(
+                    "Epoch: %2d, Step: %5d / %5d, Train loss: %.4f"
+                    % (epoch + 1, i + 1, len(train_loader), running_loss / 2000)
+                )
                 running_loss = 0.0
+
+        model.eval()
+        running_loss = 0.0
+        with torch.no_grad():
+            for i, data in enumerate(test_loader, 0):
+                # get the inputs; data is a list of [inputs, labels]
+                inputs, labels = data
+                inputs = inputs.cuda()
+                labels = labels.cuda()
+
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+
+                # print statistics
+                running_loss += loss.item()
+                if i % 2000 == 1999:  # print every 2000 mini-batches
+                    print(
+                        "Epoch: %2d, Test loss: %.4f" % (epoch + 1, running_loss / 2000)
+                    )
+                    running_loss = 0.0
 
     print("Finished Training")
 
